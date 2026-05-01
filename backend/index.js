85cb4c0ab24c39c374b29810de42e17567ef2fb7
Naveen Kumar <chaudharynaveen377@gmail.com>
2026-05-01 14:30:15 +0530

    Fix CORS + debug auth

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.options("*", cors());
 
 
 // File uploads (static)
